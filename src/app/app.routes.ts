7ed7674b579373a84e3f8a5bdc728ed21f0435fb
Luis Monge <ingeniero@luismonge.com.mx>
2018-04-27 00:01:42 -0700

    Se agregaron las rutas básicas para navegar

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import {RouterModule,Routes} from '@angular/router';
+import {AboutComponent}  from './components/about/about.component';
+import {ItemComponent}  from './components/item/item.component';
+import {BodyComponent}  from './components/body/body.component';
+
+
+const app_routes:Routes=[
+{path:'about',component:AboutComponent},
+{path:'item',component:ItemComponent},
+{path:'body',component:BodyComponent},
+{path:'**', pathMatch:'full',redirectTo:'body'}
+];
+export const app_routing=RouterModule.forRoot(app_routes,{useHash:true});
